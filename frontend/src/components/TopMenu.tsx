313ca6a33a889aec687967227bd82c2db8e9d64c
Iamhuman02 <pangpang47096@gmail.com>
2024-03-24 20:13:15 +0700

    fix top margin

--- frontend/src/components/TopMenu.tsx
+++ frontend/src/components/TopMenu.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function TopMenu () {
     return (
-        <div className="h-[11%] w-full flex" style={{ backgroundColor: '#482B11', position: 'fixed', top: 0, left: 0, right: 0, zIndex: 30 }}>
+        <div className="h-[8%] w-full flex" style={{ backgroundColor: '#482B11', position: 'fixed', top: 0, left: 0, right: 0, zIndex: 30 }}>
             <Image src={'/img/logo.png'}  alt='logo' width={0} height={0} sizes="100vh" className='h-[70%] w-auto'/>
             <TopMenuItem title='Home' pageRef='/home'/>
             <TopMenuItem title='Booking' pageRef='/booking/'/>
