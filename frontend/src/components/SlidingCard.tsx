cbbcef6961d25a21ac12627394ccc045d52d8d58
yoghurted <poomss2547@gmail.com>
2024-03-26 03:35:28 +0700

    add swiper navigate+pagination

--- frontend/src/components/SlidingCard.tsx
+++ frontend/src/components/SlidingCard.tsx
@@ -1,13 +1,21 @@
 'use client'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css'
+import 'swiper/css/bundle'
+
+import 'swiper/css/pagination';
+import './navigation.css'
+import './pagination.css'
+
 
 import getCampgrounds from "@/libs/getCampgrounds";
 import CampgroundCatalog from "@/components/CampgroundCatalog";
 import { Suspense } from "react";
 import { LinearProgress } from "@mui/material";
 import Card from './Card';
+
 
 export default async function SlidingCard() {
 
@@ -17,23 +25,31 @@ export default async function SlidingCard() {
   
     return(
       <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
       slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
+      navigation
+      pagination={{
+        dynamicBullets: true,
+        clickable: true,
+        //currentClass: 'pagination',
+        //el: '.swiper-pagination-custom'
+      }}
+    
     >
+      <div className=''>
     {
      campgrounds.data.map((campgroundItem:CampgroundItem)=>(
-      <SwiperSlide>
+      <SwiperSlide >
         <Suspense fallback={<p>Loading...<LinearProgress/></p>}>
           <Card campgroundName={campgroundItem.name} imgSrc={`/img/${campgroundItem.name}.jpg`}></Card>
-       
+          
         </Suspense>
       </SwiperSlide>
      )) 
      }
+     </div>
       
-    
     </Swiper>
     )
 }
