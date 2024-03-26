a814622458419891aa95a067d8f8510119ee66aa
Iamhuman02 <pangpang47096@gmail.com>
2024-03-27 02:54:29 +0700

    add booking alert

--- frontend/src/app/booking/campgroundCatalog2.tsx
+++ frontend/src/app/booking/campgroundCatalog2.tsx
@@ -1,17 +1,37 @@
-"use client"
-import { MenuItem, Select } from "@mui/material";
+"use client";
+import { MenuItem, Select, InputLabel } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
 import { useState } from "react";
 import { CampgroundJson } from "../../../interface";
 import { CampgroundItem } from "../../../interface";
-export default function CampgroundCatalog2({campgroundJson , onCampgroundChange} : {campgroundJson:CampgroundJson , onCampgroundChange : Function}){
-    const [selectedCampgroundid, setSelectedCampgroundid] = useState<string|null>(null);
-    return(
-        <Select value = {selectedCampgroundid} onChange={(e) => {setSelectedCampgroundid(e.target.value),(onCampgroundChange(e.target.value))}} >
-            {
-                campgroundJson.data.map((campground : CampgroundItem ) => (
-                    <MenuItem  value={campground._id}>{campground.name}</MenuItem>
-                ))    
-            }
+export default function CampgroundCatalog2({
+  campgroundJson,
+  onCampgroundChange,
+}: {
+  campgroundJson: CampgroundJson;
+  onCampgroundChange: Function;
+}) {
+  const [selectedCampgroundid, setSelectedCampgroundid] = useState<
+    string | null
+  >(null);
+  return (
+    <div>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+        <InputLabel id="demo-simple-select-standard-label">
+          Select Campground
+        </InputLabel>
+        <Select
+          value={selectedCampgroundid}
+          onChange={(e) => {
+            setSelectedCampgroundid(e.target.value),
+              onCampgroundChange(e.target.value);
+          }}
+        >
+          {campgroundJson.data.map((campground: CampgroundItem) => (
+            <MenuItem value={campground._id}>{campground.name}</MenuItem>
+          ))}
         </Select>
-    )
+      </FormControl>
+    </div>
+  );
 }
--- frontend/src/app/booking/page.tsx
+++ frontend/src/app/booking/page.tsx
@@ -1,13 +1,15 @@
 "use client"
-import { useEffect, useState } from 'react';
-import { useSession } from 'next-auth/react';
-import createBooking from '@/libs/createBooking';
-import LocationDateReserve from '@/components/LocationDateReserve';
-import { TextField, Button, MenuItem, Select } from '@mui/material';
-import styled from 'styled-components';
-import CampgroundCatalog2 from './campgroundCatalog2';
-import getCampgrounds from '@/libs/getCampgrounds';
-import { CampgroundJson } from '../../../interface';
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import createBooking from "@/libs/createBooking";
+import LocationDateReserve from "@/components/LocationDateReserve";
+import { TextField, Button } from "@mui/material";
+import styled from "styled-components";
+import CampgroundCatalog2 from "./campgroundCatalog2";
+import getCampgrounds from "@/libs/getCampgrounds";
+import { CampgroundJson } from "../../../interface";
+import Alert from '@mui/material/Alert';
+
 const WhiteBorderTextField = styled(TextField)`
   & label.Mui-focused {
     color: white;
@@ -26,28 +28,34 @@ export default function Bookings() {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedCampground, setSelectedCampground] = useState<CampgroundJson|null>(null);
+  const [selectedCampground, setSelectedCampground] =
+    useState<CampgroundJson | null>(null);
   const [selectedCampgroundid, setSelectedCampgroundid] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
   useEffect(() => {
     const fetchCampgrounds = async () => {
       try {
         const fetchedCampgrounds = await getCampgrounds();
         setSelectedCampground(fetchedCampgrounds);
-      } catch (error) {
-      }
+      } catch (error) {}
     };
     fetchCampgrounds();
-  })
-  if(!selectedCampground) return null;
-  // const campgrounds = await getCampgrounds();
+  }, []);
+
+  if (!selectedCampground) {
+    return null;
+  }
+
   const handleBooking = async () => {
     try {
       setLoading(true);
       if (!session || !session.user.token) {
-        console.error('Session or user token not available');
+        console.error("Session or user token not available");
         return;
       }
-      // Assuming you want to select the first campground by default if available
       await createBooking(
         selectedCampgroundid,
         session.user._id,
@@ -55,39 +63,55 @@ export default function Bookings() {
         new Date().toISOString(),
         session.user.token
       );
-      console.log('Booking created successfully');
+      setError(null);
+      setAlertMessage("Booking created successfully");
     } catch (error) {
-      console.error('Error creating booking:', error);
+      setError("Error creating booking:" + error);
+      setAlertMessage("Error creating booking:" + error);
     } finally {
+      setShowAlert(true);
       setLoading(false);
+      setTimeout(() => {
+        setShowAlert(false);
+        setAlertMessage("");
+      }, 3000);
     }
   };
-  
-  
+
   return (
-    <main className="w-full flex flex-col items-center space-y-4">
+    <main className="w-full flex flex-col items-center space-y-4 relative">
       {session && session.user.token ? (
         <>
-          <div className="w-full max-w-lg space-y-2 mt-8" style={{ backgroundColor: '#576453' }}>
-            <CampgroundCatalog2 campgroundJson={selectedCampground} onCampgroundChange={(value:string) => {setSelectedCampgroundid(value)}}/>
-            <LocationDateReserve 
-              onDateSelect={setSelectedDate}
-            />
+          <div className="w-full max-w-lg space-y-2 mt-8 relative" style={{ backgroundColor: "#576453" }}>
+            <div className="flex justify-center">
+              <CampgroundCatalog2
+                campgroundJson={selectedCampground}
+                onCampgroundChange={(value: string) => {
+                  setSelectedCampgroundid(value);
+                }}
+              />
+            </div>
+            <LocationDateReserve onDateSelect={setSelectedDate} />
           </div>
           <div className="w-full max-w-lg space-y-2">
             <Button
               variant="contained"
               className="w-full"
-              style={{ backgroundColor: '#576453' }}
+              style={{ backgroundColor: "#576453" }}
               onClick={handleBooking}
               disabled={loading}
             >
-              {loading ? 'Booking...' : 'Book'}
+              {loading ? "Booking..." : "Book"}
             </Button>
           </div>
+          {showAlert && (
+            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
+              <Alert severity={error ? "error" : "success"}>{alertMessage}</Alert>
+            </div>
+          )}
         </>
       ) : (
-        <p>Loading...</p> 
+        <p>Loading...</p>
       )}
     </main>
   );
