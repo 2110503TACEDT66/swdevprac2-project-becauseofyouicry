1de52b582434ffaca14ffe7542092621bd0eb3b9
Khimmybubiee <Kimmaphat@gmail.com>
2024-03-26 20:29:54 +0700

    login 100% register 100%

--- frontend/src/app/api/auth/login/page.tsx
+++ frontend/src/app/api/auth/login/page.tsx
@@ -1,35 +1,38 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import { NextPage } from 'next';
+import React, { useState } from 'react';
 import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { Link } from '@mui/material';
-import styles from './LoginPage.module.css'; 
+import styles from './LoginPage.module.css';
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [router, setRouter] = useState<any>(null); // State to hold router instance
 
-  useEffect(() => {
-    // Import useRouter only when mounted on the client-side
-    const nextRouter = require('next/router');
-    setRouter(nextRouter);
-  }, []);
+  const router = useRouter();
 
-  const handleLogin = async () => {
-    const result = await signIn('credentials', {
-      email,
-      password,
-      redirect: false
-    });
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    if (result?.error) {
-      setError(result.error);
-    } else {
-      if (router) {
-        router.push('/booking');
+    try {
+      const result = await signIn('credentials', {
+        email,
+        password,
+        redirect: false
+      });
+
+      if (result?.error) {
+        setError(result.error);
+      } else {
+        if (router) {
+          alert('Login Success!')
+          router.push('/');
+        }
       }
+    } catch (error) {
+      console.error('Error during login:', error);
+      setError('An error occurred during login.');
     }
   };
 
@@ -39,9 +42,11 @@ const LoginPage: React.FC = () => {
         <div className="bg-white p-8 rounded shadow-md w-80 relative z-10 mt-20">
           <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
           {error && <div className="text-red-500 mb-4 text-center">{error}</div>}
-          <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }} className="space-y-4">
+          <form onSubmit={handleLogin} className="space-y-4">
             <div>
-              <label htmlFor="email" className="block mb-1">Email:</label>
+              <label htmlFor="email" className="block mb-1">
+                Email:
+              </label>
               <input
                 type="email"
                 id="email"
@@ -51,7 +56,9 @@ const LoginPage: React.FC = () => {
               />
             </div>
             <div>
-              <label htmlFor="password" className="block mb-1">Password:</label>
+              <label htmlFor="password" className="block mb-1">
+                Password:
+              </label>
               <input
                 type="password"
                 id="password"
@@ -60,11 +67,13 @@ const LoginPage: React.FC = () => {
                 className="w-full border rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
               />
             </div>
-          
-            <button type="submit" className="w-full inline-flex items-center justify-center h-12 border border-green-500 rounded-full bg-green-500 text-white focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 hover:bg-green-600 transition duration-300">
+
+            <button
+              type="submit"
+              className="w-full inline-flex items-center justify-center h-12 border border-green-500 rounded-full bg-green-500 text-white focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 hover:bg-green-600 transition duration-300"
+            >
               Login
             </button>
-
           </form>
           <div className="mt-4 text-center">
             <Link href="/api/auth/register">
